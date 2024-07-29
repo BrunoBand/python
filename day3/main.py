187a846b9122ae6b1b3a59045a52403eb5aabb44
BrunoBand <brunorochaban@gmail.com>
2024-07-29 18:48:12 -0300

    leap year

--- day3/main.py
+++ day3/main.py
@@ -1,18 +1,16 @@
-# Enter your height in meters e.g., 1.55
-height = float(input())
-# Enter your weight in kilograms e.g., 72
-weight = int(input())
+# Which year do you want to check?
+year = int(input())
 # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-bmi = weight / height ** 2
-if bmi < 18.5:
-  print(f"Your BMI is {bmi}, you are underweight")
-elif bmi < 25:
-  print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi < 30:
-  print(f"Your BMI is {bmi}, you are slightly overweight.")
-elif bmi < 35:
-  print(f"Your BMI is {bmi}, you are obese.")
+# Write your code below this line 👇
+
+if (year % 4) == 0:
+    if(year % 100) == 0:
+        if(year % 400) == 0:
+            print("Leap year")
+        else:
+            print("Not leap year") 
+    else:
+        print("Leap year")
 else:
-  print(f"Your BMI is {bmi}, you are clinically obese.")
+    print("Not leap year")
