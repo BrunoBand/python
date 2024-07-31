2e0eb4927505f7a32b0a66bc84c475494eeb4c1f
BrunoBand <brunorochaban@gmail.com>
2024-07-31 19:11:19 -0300

    students

---
+++ day5/exercisestudents.py
@@ -0,0 +1,19 @@
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+  
+# Write your code below this row 👇
+total_height = 0
+number_of_students = len(student_heights)
+average_height = 0
+
+for student in student_heights:
+    total_height += int(student)
+
+average_height = total_height / number_of_students
+
+print(f"total height = {total_height}")
+print(f"number of students = {number_of_students}")
+print(f"average height = {round(average_height)}")
