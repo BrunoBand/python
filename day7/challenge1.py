26127f0851a73187fdc8597ad6f1242dc414dfd9
BrunoBand <brunorochaban@gmail.com>
2024-08-05 15:50:01 -0300

    player won

--- day7/challenge1.py
+++ day7/challenge1.py
@@ -5,18 +5,36 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-chosen_word = word_list[random.randint(0, len(word_list)-1)]
+chosen_word = random.choice(word_list)
+chosen_word_len = len(chosen_word)
 
 print(chosen_word)
-print(len(chosen_word))
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input("Letter?").lower()
+placeholder = ""
+for i in range(chosen_word_len):
+    placeholder += "_"
+print(placeholder)
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+game_over = False
+
+correct_letters = []
+while not game_over:
+    display = ''
+    guess = input("Guess a letter: ").lower()
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You Win!")
+
