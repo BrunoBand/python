e54296fb96e9897ed9b8fa065eda8414787d7540
BrunoBand <brunorochaban@gmail.com>
2024-08-06 22:58:31 -0300

    final

--- day8/caeser_cipher.py
+++ day8/caeser_cipher.py
@@ -1,29 +1,22 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("encode or decode? ").lower()
-text = input("message: ").lower()
-shift = int(input("shift number: "))
-
-def encrypt(original_text, shift_amount):
-    encrypted = ''
+def caesar(original_text, shift_amount, encode_decode):
+    output_text = ''
+    if encode_decode == "decode":
+        shift_amount *= -1
     for letter in original_text:
-        position = alphabet.index(letter) + shift_amount
-        position %= len(alphabet)
-        encrypted += alphabet[position]
-    print(encrypted)
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(output_text)
 
-def decrypt(original_text, shift_amount):
-    decrypted = ''
-    for letter in original_text:
-        position = alphabet.index(letter) - shift_amount
-        position %= len(alphabet)
-        decrypted += alphabet[position]
-    print(decrypted)
-
-def caesar(text, shift, direction):
-    if direction == "encode":
-        encrypt(text, shift)
-    else:
-        decrypt(text, shift)
-
-caesar(text, shift, direction)
+continuar = "yes"
+while continuar == "yes":
+    direction = input("encode or decode? ").lower()
+    text = input("message: ").lower()
+    shift = int(input("shift number: "))
+    caesar(original_text=text, shift_amount=shift, encode_decode=direction)
+    continuar = input("Restart? yes or not: " )
